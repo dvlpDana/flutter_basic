2edf847aa13e2458b2a600780f7e6a0929c0a606
DahnBee Shim <colleksql@8snippet.com>
2023-10-03 17:31:45 +0900

    flutter stateful directory 추가

---
+++ flutter_builder/lib/screen/home_screen.dart
@@ -0,0 +1,70 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = TextStyle(
+      fontSize: 16.0,
+    );
+
+    return Scaffold(
+      body: FutureBuilder(
+        future: getNumber(),
+        builder: (context, snapshot) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  'FutureBuilder',
+                  style: textStyle.copyWith(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 20.0,
+                  ),
+                ),
+                Text(
+                  'ContState: ${snapshot.connectionState}',
+                  style: textStyle,
+                ),
+                Text(
+                  'Data: ${snapshot.data}',
+                  style: textStyle,
+                ),
+                Text(
+                  'Error: ${snapshot.error}',
+                  style: textStyle,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+
+                    });
+                  },
+                  child: Text('setState'),
+                )
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Future<int> getNumber() async {
+    await Future.delayed(Duration(seconds: 3));
+
+    final random = Random();
+
+    return random.nextInt(100);
+  }
+}
